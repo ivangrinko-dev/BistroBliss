0bea8879b410b2a2cd51710784cf02d1799af540
vanyaolyagrinko-work <vanya.grinko.82@mail.ru>
2023-11-17 16:53:34 +0300

    style

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -5,27 +5,34 @@ export default function Header() {
         <>
             <div className={style.wrapper}>
                 <div className={style.topBar}>
+                    <div className={style.info}>
 
-                    <div className={style.phone}>
-                        <div className={style.img_phone}></div>
-                        <h1>(414) 857 - 0107</h1>
+                        <div className={style.phone}>
+                            <div className={style.img_phone}></div>
+                            <h1>(414) 857 - 0107</h1>
+                        </div>
+
+                        <div className={style.mail}>
+                            <div className={style.img_mail}></div>
+                            <h1>yummy@bistrobliss</h1>
+                        </div>
+
                     </div>
-                    
-                    <div className={style.mail}>
-                        <div className={style.img_mail}></div>
-                        <h1>yummy@bistrobliss</h1>
+
+                    <div className={style.social}>
+                        <div className={style.img_1}></div>
+                        <div className={style.img_2}></div>
+                        <div className={style.img_3}></div>
+                        <div className={style.img_4}></div>
                     </div>
+
 
                 </div>
 
 
 
-
-
-                <div className={style.header}></div>
-
-
             </div>
+
         </>
     )
 }
