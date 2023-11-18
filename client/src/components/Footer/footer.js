8e8a5e9156ef9c7eb3b6d17ded1c43b13737cb0d
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-11-18 08:45:02 +0300

    start project

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,7 +1,50 @@
-export default function Footer(){
-    return(
-        <>
-        <h1>FOOTER</h1>
-        </>
-    )
+import style from "../Footer/footer.module.css";
+
+export default function Footer() {
+  return (
+    <>
+      <div className={style.wrapper}>
+        <div className={style.conteiner}>
+          <div className={style.logotip}>
+            <div className={style.barist}>
+              <div className={style.img_1}></div>
+              <h1>Bistro Bliss</h1>
+            </div>
+            <p>
+              In the new era of technology we look a in the future with
+              certainty and pride to for our company and.
+            </p>
+          </div>
+          <div className={style.info}>
+            <div className={style.pages}>
+              <h2>Pages</h2>
+              <p>Home</p>
+              <p>About</p>
+              <p>Menu</p>
+              <p>Pricing</p>
+              <p>Blog</p>
+              <p>Contact</p>
+              <p>Delivery</p>
+            </div>
+            <div className={style.utility}>
+              <h2>Utility Pages</h2>
+              <p>Start Here</p>
+              <p>Styleguide</p>
+              <p>Password Protected</p>
+              <p>404 Not Found</p>
+              <p>Licenses</p>
+              <p>Changelog</p>
+              <p>View More</p>
+            </div>
+          </div>
+          <div className={style.foto}></div>
+        </div>
+      </div>
+      <div className={style.block}>
+        <div className={style.line}></div>
+
+        <h1>Copyright © 2023 Hashtag Developer. All Rights Reserved</h1>
+      </div>
+    </>
+  );
 }
